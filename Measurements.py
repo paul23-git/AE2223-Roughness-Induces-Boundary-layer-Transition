Big bugfix in scaling
Scaling is now fixed at 1.75 for all measurements

Measurements.py
@@ -75,10 +75,9 @@ class measurement(object):
         Rex = m.V*m.chord/v
         x0 = self.data.cols - self.slice[1][0]
         x1 = self.data.cols - self.slice[1][1] 
-        xarr = np.array(range(x1, x0) ) + 1/(2*self.scale)
-        print("-----------", self.scale, self.slice, self.data.cols, x0, x1)
-        print(xarr)
-        return Rex*self.scale/1000* xarr
+        print("=====", self.data.cols, x1, x0, self.point[0], self.leading_edge_distance -(self.data.cols - x1 - self.point[0])/m.scale)
+        xarr = np.array(range(x1, x0) ) + 0.5
+        return Rex*((xarr)/(m.scale*1000)  )
         # calculate and insert other flow & measurement parameters here!
     @property
     def cp(self):
@@ -177,7 +176,17 @@ class measurement(object):
             self.data.saveQ()
     def isLoaded(self):
         return self.data is not None
+
+    def to_absolute_pos(self, relative_pos):
+        return tuple(map(operator.add,relative_pos, self.offsets))
     
+    def to_relative_pos(self, absolute_pos):
+        return tuple(map(operator.sub,absolute_pos, self.offsets))
+    
+    def to_point_pos(self, absolute_pos):
+        return tuple(map(operator.sub,absolute_pos, self.point))
+        
+
     def __repr__(self):
         return "measure" + str((self.filepath, str(self.leading_edge_distance)))
     
@@ -192,7 +201,7 @@ class simple_measurement(object):
     Measurement object contains all data relevant to a measurement
     """ 
     
-    def __init__(self, measurement, data = None, index_lambda = None):
+    def __init__(self, measurement, data = None, get_lambda = None):
         """
         Initializes a measurement object, keeps track of all measurements and other data
         @param filepath: filepath
@@ -223,13 +232,15 @@ class simple_measurement(object):
         self.offsets = measurement.offsets[:-1]
         self.ind = -1
         self.Ktresh_hold = measurement.Ktresh_hold
-        if index_lambda is not None:
-            ind = index_lambda(data)
         
         if data is None:
-            self._data = measurement.data.data[:,:,self.ind]
+            data = measurement.data
+        
+        if get_lambda is not None:
+            self._data = get_lambda(data)
         else:
-            self._data = data[:,:,self.ind]
+            self._data = data[:,:,-1]
+
         self.reynolds = measurement.reynolds
         self.st_lam = measurement.st_lam
         self.st_turn = measurement.st_turb

analyses.py
@@ -2,6 +2,7 @@ from collections import namedtuple
 import os.path
 import numpy as np
 from scipy import ndimage
+import matplotlib.pyplot as plt
 
 import Measurements
 def gaussian2d(rsquared, sigma):
@@ -30,23 +31,56 @@ def addBoxBlur(data):
 def getColumnLine(data2d):
     ret = np.empty(data2d.shape[1])
     for i, column in enumerate(data2d.T):
-        ret[i] = np.mean(column)
+        ret[i] = np.nanmean(column)
     return ret
 def getRowLine(data2d):
     ret = np.empty(data2d.shape[0])
     for i, column in enumerate(data2d):
-        ret[i] = np.mean(column)
+        ret[i] = np.nanmean(column)
     return ret
 
 def averageTotal(data2d):
-    return np.mean(data2d)
+    return np.nanmean(data2d)
 
-def plotData(reduced_measurements):
+def MakeRoughnessNan(data2d, point_px, radius_px):
+    rsq = radius_px**2
+    for index, x in np.ndenumerate(data2d):
+        i = (index[1] - point_px[0], index[0] - point_px[1])
+        if i[0]**2 + i[1]**2 <= rsq:
+            data2d[index[0], index[1]] = float('nan')
+    return data2d
+    
+    
+        
+def groupMeasurementData(m_data_list):
+    d = {}
+    for m_data in m_data_list:
+        m = m_data[0]
+        name = m.filepath[:-9]
+        data = m_data[1]
+        if name in d:
+            d[name].append((data, m))
+        else:
+            d[name] = [(data, m)]
+    return d
+def combineMeasurementData(m_data_list):
+    d = groupMeasurementData(m_data_list)
+    for i in d:
+        v = d[i]
+        d[i] = (np.mean(v, 1), v[1])
+    
+    return [d.items()]
+    
+
+
+
+def plotData(reduced_measurements, simple_display = True):
     colors = ["b", "g", "r", "c", "m", "y"]
     f1 = plt.figure()
     ax1 = f1.add_subplot("111")
-    
-    d = [(m, analyses.getColumnLine(m.data)) for m in reduced_measurements]
+    for m in reduced_measurements:
+        MakeRoughnessNan(m.data, m.to_relative_pos(m.point), m.size*m.scale+1)
+    d = [(m, getColumnLine(m.data)) for m in reduced_measurements]
     g = groupMeasurementData(d)
     #d = [(m, analyses.averageTotal(m.data)) for m in reduced_measurements]
 
@@ -58,7 +92,13 @@ def plotData(reduced_measurements):
         for j in i:
             d = j[0]
             m = j[1]
-            xval = np.array(range(len(d),0,-1)) / m.scale
+            o = 0;
+            if not simple_display:
+                o = 320-m.data.shape[1]-m.offsets[0]
+            else:
+                print("-=-==-", (m.data.shape[1]+m.offsets[0]-m.point[0]))
+                o = -(m.data.shape[1]+m.offsets[0]-m.point[0])
+            xval = (np.array(range(len(d),0,-1))+o) / m.scale
             if added_to_leg:
                 plt.plot(xval, d, color=c)
             else:
@@ -74,7 +114,7 @@ def plotData(reduced_measurements):
     f2 = plt.figure()
     ax2 = f2.add_subplot("111")
     
-    d = [(m, analyses.getRowLine(m.data)) for m in reduced_measurements]
+    d = [(m, getRowLine(m.data)) for m in reduced_measurements]
     g = groupMeasurementData(d)
     #d = [(m, analyses.averageTotal(m.data)) for m in reduced_measurements]
 
@@ -86,7 +126,7 @@ def plotData(reduced_measurements):
         for j in i:
             d = j[0]
             m = j[1]
-            xval = np.array(range(len(d),0,-1)) / m.scale
+            xval = (np.array(range(len(d),0,-1))) / m.scale
             if added_to_leg:
                 plt.plot(xval, d, color=c)
             else:
@@ -97,3 +137,4 @@ def plotData(reduced_measurements):
     ax2.set_ylabel("K $[\cdot]$")
     ax2.set_xlabel("Vertical position $[mm]$")
     ax2.legend()
+    return ax1, ax2

ptw.py
@@ -192,7 +192,6 @@ class ptw_file(object):
 		return self._data
 	@staticmethod
 	def matlab_convert(Im, tint, Tcam, eps):
-		print(tint, Tcam, eps)
 		if(np.round(tint*1.e6) == 400):
 		    print("--- Converting (Matlab 400 microsec) ---")
 		    elambda=np.array([2498026.51787585,1784.71556672801,6.67171636375203e-12,3215.59874368947,493144.349437419])
@@ -209,7 +208,7 @@ class ptw_file(object):
 		return Tm
 	
 	def matlab_convert_helper(self):
-		return ptw_file.matlab_convert(self.dataRaw(),self.integration, 28+273.15, 0.86)
+		return ptw_file.matlab_convert(self.dataRaw(),self.integration, 25+273.15, 0.86)
 		#return self.python_convert()
 	def calcTempML_U(self):
 		return ptw_file.matlab_convert(self._undisturbedData,self.integration, 22.4+273.15, 0.86)
@@ -337,7 +336,6 @@ class ptw_file(object):
 		rho_static = 1190
 		c = 1200
 		k = 0.02
-		print(rho_static, c, k)
 		for x in range(v.shape[0]):
 			print ("calculating column: ", x)
 			for y in range((self.ml_delta_temp.shape[1])):